4e5f44305d001fbc2493defbd472039471c072e2
hadipsy27 <hadipsy27@gmail.com>
2020-03-01 23:25:24 +0700

    fix tema admin blank page

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -42,8 +42,8 @@
 			<hr class="sidebar-divider my-0">
 
 			<!-- Nav Item - Dashboard -->
-			<li class="nav-item">
-				<a class="nav-link" href="index.html">
+			<li class="nav-item active">
+				<a class="nav-link active" href="index.html">
 					<i class="fas fa-fw fa-tachometer-alt"></i>
 					<span>Dashboard</span></a>
 			</li>
@@ -99,13 +99,13 @@
 			</div>
 
 			<!-- Nav Item - Pages Collapse Menu -->
-			<li class="nav-item active">
+			<li class="nav-item">
 				<a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
 					aria-controls="collapsePages">
 					<i class="fas fa-fw fa-folder"></i>
 					<span>Pages</span>
 				</a>
-				<div id="collapsePages" class="collapse show" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+				<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
 					<div class="bg-white py-2 collapse-inner rounded">
 						<h6 class="collapse-header">Login Screens:</h6>
 						<a class="collapse-item" href="login.html">Login</a>
@@ -114,7 +114,7 @@
 						<div class="collapse-divider"></div>
 						<h6 class="collapse-header">Other Pages:</h6>
 						<a class="collapse-item" href="404.html">404 Page</a>
-						<a class="collapse-item active" href="blank.html">Blank Page</a>
+						<a class="collapse-item" href="blank.html">Blank Page</a>
 					</div>
 				</div>
 			</li>
